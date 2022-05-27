1d913473ce2c6877b0d6893c4c8c1e58b188214c
vinay <vinayakumarc11@gmail.com>
2022-05-27 14:00:16 +0530

    committing Test2 to Branchx

---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name="vinay";
+		if(name=="vinay")
+		{
+			System.out.println(name);
+		}
+		else
+		{
+			System.out.println("Name not found");
+		}
+	}
+
+}
