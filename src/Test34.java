5434bdafb6e804d743d757678736ba0ca6e0f7b2
mahesh <vinayc10@gmail.com>
2022-05-29 22:37:04 +0530

    Modified test34

--- src/Test34.java
+++ src/Test34.java
@@ -5,7 +5,7 @@ public class Test34 {
 		// TODO Auto-generated method stub
 
 		String name="vinay";
-		if(name=="vinay" || name=="mahesh")
+		if(name=="vinay" || name=="mahesh" || name=="anil")
 		{
 			System.out.println(name);
 			System.out.println(name);
