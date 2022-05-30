378cd369d499bdd0f45381a7686531bf644bde87
seleniumthoughts <106177499+seleniumthoughts@users.noreply.github.com>
2022-05-30 12:34:12 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 		// TODO Auto-generated method stub
 
 		String name="vinay";
-		if(name=="vinay" || name=="B")
+		if(name=="vinay" || name=="B" || name=="C")
 		{
 			System.out.println(name);
 		}
